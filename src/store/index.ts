99e69e6c395e3a6b5065ca4fba39137bea3c0d59
Volodymyr Yesypenko <volodymyrye@tahometer.com>
2020-12-19 19:49:42 +0200

    implement GetById request

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,14 +6,9 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    numOfAssignments: 1,
     publications: [],
   },
   mutations: {
-    increment(state) {
-      state.numOfAssignments += 1;
-      console.log(state.numOfAssignments);
-    },
     getData(state) {
       axios
         .get('http://localhost:5000/api/publications')
