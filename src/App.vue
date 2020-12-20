4a594e5fe53697564babe3702c97274f4f15b9db
ThatMadMf <maxdarksiders@gmail.com>
2020-12-21 01:41:18 +0200

    Remove unused components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/test">Test</router-link> |
       <router-link to="/publications">Publications</router-link>
     </div>
     <router-view/>
